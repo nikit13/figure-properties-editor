c88481d743f90dcc70dc08a9c7a51036b2608d95
Kurasov Nikita <nikitakur1993@gmail.com>
2016-08-27 20:41:37 +0300

    add active figure

--- ru.spb.nkurasov.figure.editor/src/main/java/ru/spb/nkurasov/figure/editor/service/FigureService.java
+++ ru.spb.nkurasov.figure.editor/src/main/java/ru/spb/nkurasov/figure/editor/service/FigureService.java
@@ -1,15 +1,22 @@
 package ru.spb.nkurasov.figure.editor.service;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import ru.spb.nkurasov.figure.editor.Figure;
 
 public interface FigureService {
-    
+
     void addFigure(Figure figure);
-    
+
     boolean removeFigure(Figure figure);
-    
+
     Collection<Figure> getFigures();
+
+    void setActiveFigure(Figure figure);
+
+    Optional<Figure> getActiveFigure();
+
+    boolean isFigureActivated();
 
 }
--- ru.spb.nkurasov.figure.editor/src/main/java/ru/spb/nkurasov/figure/editor/service/internal/FigureServiceImpl.java
+++ ru.spb.nkurasov.figure.editor/src/main/java/ru/spb/nkurasov/figure/editor/service/internal/FigureServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import ru.spb.nkurasov.figure.editor.Figure;
 import ru.spb.nkurasov.figure.editor.service.FigureService;
@@ -11,6 +12,8 @@ import ru.spb.nkurasov.figure.editor.service.FigureService;
 class FigureServiceImpl implements FigureService {
 
     private final List<Figure> figures = new ArrayList<Figure>();
+    
+    private Optional<Figure> activeFigure = Optional.empty();
     
     FigureServiceImpl() {
     }
@@ -34,6 +37,25 @@ class FigureServiceImpl implements FigureService {
     @Override
     public Collection<Figure> getFigures() {
         return Collections.unmodifiableList(figures);
+    }
+
+    @Override
+    public void setActiveFigure(Figure figure) {
+        if (figure != null && !figures.contains(figure)) {
+            throw new IllegalStateException("figure not registered");
+        }
+        
+        activeFigure = Optional.ofNullable(figure);
+    }
+
+    @Override
+    public Optional<Figure> getActiveFigure() {
+        return activeFigure;
+    }
+
+    @Override
+    public boolean isFigureActivated() {
+        return activeFigure.isPresent();
     }
     
 }
