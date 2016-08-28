1a06614b7b8693872aef7974616e7ea5277b0daf
Kurasov Nikita <nikitakur1993@gmail.com>
2016-08-28 13:36:46 +0300

    add handlers stubs

---
+++ ru.spb.nkurasov.figure.editor.ui/src/main/java/ru/spb/nkurasov/figure/editor/ui/actions/AddFigureHandler.java
@@ -0,0 +1,14 @@
+package ru.spb.nkurasov.figure.editor.ui.actions;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class AddFigureHandler extends AbstractHandler {
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        // FIXME
+        throw new UnsupportedOperationException();
+    }
+}
